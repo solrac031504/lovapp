7655cc31026eab76d71b673a56608594a78865ec
solrac031504 <carlos.gonzalez031504@gmail.com>
2026-06-03 21:56:33 -0400

    Updating comment

--- loveapp/models.py
+++ loveapp/models.py
@@ -3,10 +3,10 @@ from datetime import datetime
 from extensions import db
 from flask_login import UserMixin
 
-"""Represents """
-
 
 class User(UserMixin, db.Model):
+    """Represents"""
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
